b5ca4ba430050b159c38fcee546ecb22a998dc6f
Sapein <Chandler182@gmail.com>
2014-08-27 18:04:49 -0500

    Fixed a major error
    I fixed a major error today, the error would cause the program to
    continously loop until you closed the program. I temporarily solved it by
    adding removing a feature and setting the total days to days(thus
    simulating only one day) it will also print out the day number it's on(up
    to the number prior to the amount you entered. Still need to work on some
    balancing. I also plan on eventually adding in individualized deer this
    wat the simulation will be semi-random in it's outcome.

--- src/com/github/populationstuff/EndingChecks.java
+++ src/com/github/populationstuff/EndingChecks.java
@@ -2,13 +2,10 @@ package com.github.populationstuff;
 
 public class EndingChecks {
 
-	public void endSimulationCheck(){
+	public static void endSimulationCheck(){
 		if(Variables.deerPopulation == 0 && Variables.offSpring == 0){
 			System.out.println("Deer Died");
 			Variables.totalDays = Variables.days;
-		}
-		if(Variables.deerPopulation > 0 || Variables.offSpring > 0){
-			Variables.totalDays = Variables.totalDays + 0;
 		}
 	}
 }
--- src/com/github/populationstuff/Formulas.java
+++ src/com/github/populationstuff/Formulas.java
@@ -1,19 +1,19 @@
 package com.github.populationstuff;
 
 public class Formulas {
+	//This simply loads up what is needed to run the calculations
+	static DeerPopulationCalculations deerPopulationCalc = new DeerPopulationCalculations(); 
+	//static EndingChecks checkEnd = new EndingChecks(); 
+	static ResourceComsumption Consumption = new ResourceComsumption(); 
+	static ResourceEffects Effects = new ResourceEffects();
+	static NegativeNumberCheck Check = new NegativeNumberCheck();
+	static ResourceRegrowth Regrowth = new ResourceRegrowth();
 	public static void doCalculations(){ //This does the Calculations
-		//This simply loads up what is needed to run the calculations
-		DeerPopulationCalculations deerPopulationCalc = new DeerPopulationCalculations(); 
-		EndingChecks checkEnd = new EndingChecks(); 
-		ResourceComsumption Consumption = new ResourceComsumption(); 
-		ResourceEffects Effects = new ResourceEffects();
-		NegativeNumberCheck Check = new NegativeNumberCheck();
-		ResourceRegrowth Regrowth = new ResourceRegrowth();
-		
-		while(Variables.totalDays != Variables.days){ //This is the Beggining of the Calculations
-			checkEnd.endSimulationCheck(); //checks to see if we should end the simulation
+	
+		while(Variables.totalDays < Variables.days){ //This is the Beggining of the Calculations
+			EndingChecks.endSimulationCheck(); //checks to see if we should end the simulation
 			deerPopulationCalc.deerPopulationCalculations(); //Does the Calculations for Deer Population
-			checkEnd.endSimulationCheck(); //Checks to see if we should end
+			EndingChecks.endSimulationCheck(); //Checks to see if we should end
 			Check.checkForNegativeNumbers(); //corrects any possible negative numbers we have
 			Consumption.FoodConsumption(); //Changes the amount of Food
 			Consumption.WaterConsumption(); //Changes the amount of water
@@ -22,9 +22,10 @@ public class Formulas {
 			Check.checkForNegativeNumbers(); //Changes any negative numbers to 0
 			Effects.lackOfFood(); //if we have 0 food then this runs
 			Effects.lackOfWater(); //if we have 0 water then this runs
-			checkEnd.endSimulationCheck(); //checks to see if the simulation should end
+			EndingChecks.endSimulationCheck(); //checks to see if the simulation should end
 			Check.checkForNegativeNumbers(); //Fizes any negative numbers we have
 			System.out.println(Variables.totalDays);
+			Variables.totalDays++;
 		}
 	}
 	public static void finalOutput(){ //This is the Final Output. 
